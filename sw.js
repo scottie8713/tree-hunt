dbcf634fc12ee44f947c7cd9c4f60ef23478687b
scottie8713 <stephanieschaap14@gmail.com>
2026-06-07 20:33:34 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'treehunt-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'treeseeker-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
